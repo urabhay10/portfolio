a14471602b7cd2f1a1f300057cbaf53a0421e6c4
Abhay Upadhyay <140256312+urabhay10@users.noreply.github.com>
2023-12-05 15:44:23 +0000

    none

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Abhay Upadhyay",
+  title: "Student & Developer",
+  email: "urabhay10@gmail.com",
+  gitHub: "urabhay10",
+  linkedIn: "abhay-upadhyay-7a7597292",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#0A192F";
+const secondaryColor = "#8892B0";
 
 const App = () => {
   return (
